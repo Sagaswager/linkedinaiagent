fbc20c1c7450f050a69cc58dcde877ea3378c504
Sagar Masand <65072024+Sagaswager@users.noreply.github.com>
2026-05-21 12:57:55 +0530

    Delete fix.py

--- fix.py
+++
@@ -1,8 +0,0 @@
-﻿with open('dashboard.html', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-text = text.replace('btn.innerHTML = \\Resend OTP in \\s\\;', 'btn.innerHTML = Resend OTP in s;')
-
-with open('dashboard.html', 'w', encoding='utf-8') as f:
-    f.write(text)
-print('Fixed!')
