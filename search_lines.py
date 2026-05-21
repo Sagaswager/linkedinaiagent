1c03eec10b65bb94672628ee1a819521a8bc71b8
Sagar Masand <65072024+Sagaswager@users.noreply.github.com>
2026-05-21 13:01:38 +0530

    Delete search_lines.py

--- search_lines.py
+++
@@ -1,6 +0,0 @@
-with open('dashboard.html', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
-for i, line in enumerate(lines, 1):
-    low = line.lower()
-    if any(x in low for x in ['connection', 'dashed', 'connections-layer', 'drawline', 'draw_line', 'stroke-dash']):
-        print(str(i) + ': ' + line.rstrip())
